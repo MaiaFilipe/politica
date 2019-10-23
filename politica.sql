bbeb9ff7b8ae296bc18f3a7776891d47403e5fd9
maiafilipe <filexicientista@gmail.com>
2019-10-23 15:36:55 -0200

    :)

--- politica.sql
+++ politica.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.5.7
 -- Dumped by pg_dump version 9.5.7
 
--- Started on 2019-10-23 12:43:21 BRST
+-- Started on 2019-10-23 15:36:25 BRST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,6 +15,23 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- TOC entry 1 (class 3079 OID 12395)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2158 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -22,7 +39,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 181 (class 1259 OID 34242)
+-- TOC entry 181 (class 1259 OID 58021)
 -- Name: publicacao; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -40,7 +57,62 @@ CREATE TABLE publicacao (
 ALTER TABLE publicacao OWNER TO postgres;
 
 --
--- TOC entry 2143 (class 0 OID 34242)
+-- TOC entry 182 (class 1259 OID 58027)
+-- Name: sq_publicacao; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE sq_publicacao
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE sq_publicacao OWNER TO postgres;
+
+--
+-- TOC entry 183 (class 1259 OID 58029)
+-- Name: sq_usuario; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE sq_usuario
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE sq_usuario OWNER TO postgres;
+
+--
+-- TOC entry 184 (class 1259 OID 58031)
+-- Name: usuario; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE usuario (
+    cpf numeric,
+    cell numeric,
+    senha character varying,
+    posicao character varying,
+    email character varying,
+    descricao character varying,
+    nome character varying,
+    nascimento character varying,
+    estado character varying,
+    id integer NOT NULL,
+    usuario character varying,
+    extensao text,
+    foto bytea,
+    tipo character varying
+);
+
+
+ALTER TABLE usuario OWNER TO postgres;
+
+--
+-- TOC entry 2147 (class 0 OID 58021)
 -- Dependencies: 181
 -- Data for Name: publicacao; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -50,7 +122,44 @@ COPY publicacao (conteudo, titulo, idp, id_usuario, foto, extensao, horario) FRO
 
 
 --
--- TOC entry 2027 (class 2606 OID 34261)
+-- TOC entry 2159 (class 0 OID 0)
+-- Dependencies: 182
+-- Name: sq_publicacao; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('sq_publicacao', 18, true);
+
+
+--
+-- TOC entry 2160 (class 0 OID 0)
+-- Dependencies: 183
+-- Name: sq_usuario; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('sq_usuario', 9, true);
+
+
+--
+-- TOC entry 2150 (class 0 OID 58031)
+-- Dependencies: 184
+-- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY usuario (cpf, cell, senha, posicao, email, descricao, nome, nascimento, estado, id, usuario, extensao, foto, tipo) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2031 (class 2606 OID 58038)
+-- Name: pk_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY usuario
+    ADD CONSTRAINT pk_id PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2029 (class 2606 OID 58040)
 -- Name: pk_idp; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -59,7 +168,7 @@ ALTER TABLE ONLY publicacao
 
 
 --
--- TOC entry 2025 (class 1259 OID 34262)
+-- TOC entry 2027 (class 1259 OID 58041)
 -- Name: fki_id_usuario; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -67,7 +176,7 @@ CREATE INDEX fki_id_usuario ON publicacao USING btree (id_usuario);
 
 
 --
--- TOC entry 2028 (class 2606 OID 34263)
+-- TOC entry 2032 (class 2606 OID 58042)
 -- Name: fk_id_usuario; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -75,7 +184,19 @@ ALTER TABLE ONLY publicacao
     ADD CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id);
 
 
--- Completed on 2019-10-23 12:43:22 BRST
+--
+-- TOC entry 2157 (class 0 OID 0)
+-- Dependencies: 7
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2019-10-23 15:36:25 BRST
 
 --
 -- PostgreSQL database dump complete
