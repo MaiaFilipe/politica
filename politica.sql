62d64462680a05eca8b242f9118fd561c4853cd8
maiafilipe <filexicientista@gmail.com>
2019-11-19 13:09:59 -0200

    Pesquisando usuários

--- politica.sql
+++ politica.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.5.7
 -- Dumped by pg_dump version 9.5.7
 
--- Started on 2019-11-13 13:43:25 BRST
+-- Started on 2019-11-19 13:09:44 BRST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,23 +15,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 1 (class 3079 OID 12395)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- TOC entry 2173 (class 0 OID 0)
--- Dependencies: 1
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -39,256 +22,73 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 181 (class 1259 OID 91815)
--- Name: comentario; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 187 (class 1259 OID 92019)
+-- Name: seguir; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE comentario (
-    comentario character varying,
-    comentador integer,
-    publicacao integer,
+CREATE TABLE seguir (
+    seguidor integer,
+    seguido integer,
     id integer NOT NULL
 );
 
 
-ALTER TABLE comentario OWNER TO postgres;
+ALTER TABLE seguir OWNER TO postgres;
 
 --
--- TOC entry 182 (class 1259 OID 91821)
--- Name: publicacao; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 2154 (class 0 OID 92019)
+-- Dependencies: 187
+-- Data for Name: seguir; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-CREATE TABLE publicacao (
-    conteudo character varying,
-    titulo character varying,
-    idp integer NOT NULL,
-    usuario integer,
-    foto bytea,
-    extensao text,
-    horario timestamp without time zone
-);
-
-
-ALTER TABLE publicacao OWNER TO postgres;
-
---
--- TOC entry 186 (class 1259 OID 91861)
--- Name: sq_comentario; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE sq_comentario
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE sq_comentario OWNER TO postgres;
-
---
--- TOC entry 183 (class 1259 OID 91827)
--- Name: sq_publicacao; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE sq_publicacao
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE sq_publicacao OWNER TO postgres;
-
---
--- TOC entry 184 (class 1259 OID 91829)
--- Name: sq_usuario; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE sq_usuario
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE sq_usuario OWNER TO postgres;
-
---
--- TOC entry 185 (class 1259 OID 91831)
--- Name: usuario; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE usuario (
-    cpf numeric,
-    cell numeric,
-    senha character varying,
-    posicao character varying,
-    email character varying,
-    descricao character varying,
-    nome character varying,
-    nascimento character varying,
-    estado character varying,
-    id integer NOT NULL,
-    usuario character varying,
-    extensao text,
-    foto bytea,
-    tipo character varying
-);
-
-
-ALTER TABLE usuario OWNER TO postgres;
-
---
--- TOC entry 2160 (class 0 OID 91815)
--- Dependencies: 181
--- Data for Name: comentario; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY comentario (comentario, comentador, publicacao, id) FROM stdin;
+COPY seguir (seguidor, seguido, id) FROM stdin;
 \.
 
 
 --
--- TOC entry 2161 (class 0 OID 91821)
--- Dependencies: 182
--- Data for Name: publicacao; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 2037 (class 2606 OID 92023)
+-- Name: pk_id_seguir; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-COPY publicacao (conteudo, titulo, idp, usuario, foto, extensao, horario) FROM stdin;
-\.
-
-
---
--- TOC entry 2174 (class 0 OID 0)
--- Dependencies: 186
--- Name: sq_comentario; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('sq_comentario', 10, true);
+ALTER TABLE ONLY seguir
+    ADD CONSTRAINT pk_id_seguir PRIMARY KEY (id);
 
 
 --
--- TOC entry 2175 (class 0 OID 0)
--- Dependencies: 183
--- Name: sq_publicacao; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- TOC entry 2034 (class 1259 OID 92035)
+-- Name: fki_seguido; Type: INDEX; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('sq_publicacao', 26, true);
-
-
---
--- TOC entry 2176 (class 0 OID 0)
--- Dependencies: 184
--- Name: sq_usuario; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('sq_usuario', 16, true);
+CREATE INDEX fki_seguido ON seguir USING btree (seguido);
 
 
 --
--- TOC entry 2164 (class 0 OID 91831)
--- Dependencies: 185
--- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 2035 (class 1259 OID 92029)
+-- Name: fki_seguidor; Type: INDEX; Schema: public; Owner: postgres
 --
 
-COPY usuario (cpf, cell, senha, posicao, email, descricao, nome, nascimento, estado, id, usuario, extensao, foto, tipo) FROM stdin;
-\.
+CREATE INDEX fki_seguidor ON seguir USING btree (seguidor);
 
 
 --
--- TOC entry 2042 (class 2606 OID 91838)
--- Name: pk_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 2039 (class 2606 OID 92030)
+-- Name: fk_seguido; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY usuario
-    ADD CONSTRAINT pk_id PRIMARY KEY (id);
-
-
---
--- TOC entry 2037 (class 2606 OID 91840)
--- Name: pk_id_comentario; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY comentario
-    ADD CONSTRAINT pk_id_comentario PRIMARY KEY (id);
+ALTER TABLE ONLY seguir
+    ADD CONSTRAINT fk_seguido FOREIGN KEY (seguido) REFERENCES usuario(id);
 
 
 --
--- TOC entry 2040 (class 2606 OID 91842)
--- Name: pk_idp; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 2038 (class 2606 OID 92024)
+-- Name: fk_seguidor; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY publicacao
-    ADD CONSTRAINT pk_idp PRIMARY KEY (idp);
+ALTER TABLE ONLY seguir
+    ADD CONSTRAINT fk_seguidor FOREIGN KEY (seguidor) REFERENCES usuario(id);
 
 
---
--- TOC entry 2038 (class 1259 OID 91843)
--- Name: fki_id_usuario; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX fki_id_usuario ON publicacao USING btree (usuario);
-
-
---
--- TOC entry 2034 (class 1259 OID 91844)
--- Name: fki_pk_comentador; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX fki_pk_comentador ON comentario USING btree (comentador);
-
-
---
--- TOC entry 2035 (class 1259 OID 91845)
--- Name: fki_pk_publicacao; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE INDEX fki_pk_publicacao ON comentario USING btree (publicacao);
-
-
---
--- TOC entry 2045 (class 2606 OID 91846)
--- Name: fk_id_usuario; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY publicacao
-    ADD CONSTRAINT fk_id_usuario FOREIGN KEY (usuario) REFERENCES usuario(id);
-
-
---
--- TOC entry 2043 (class 2606 OID 91851)
--- Name: pk_comentador; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY comentario
-    ADD CONSTRAINT pk_comentador FOREIGN KEY (comentador) REFERENCES usuario(id);
-
-
---
--- TOC entry 2044 (class 2606 OID 91856)
--- Name: pk_publicacao; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY comentario
-    ADD CONSTRAINT pk_publicacao FOREIGN KEY (publicacao) REFERENCES publicacao(idp);
-
-
---
--- TOC entry 2172 (class 0 OID 0)
--- Dependencies: 7
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
--- Completed on 2019-11-13 13:43:25 BRST
+-- Completed on 2019-11-19 13:09:44 BRST
 
 --
 -- PostgreSQL database dump complete
